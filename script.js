d54f09568f69df5f2ecc59ed00e40f2970d3ce68
GabrielBR90 <139515580+GabrielBR90@users.noreply.github.com>
2025-02-10 23:37:01 -0300

    script.js

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const naturezas = {
-        "Grupo A - Contra a Pessoa e a Vida": [
-            { code: "A01", name: "Homicídio culposo" },
-            { code: "A02", name: "Homicídio doloso" },
-            { code: "A03", name: "Homicídio tentado" },
-            { code: "A05", name: "Agressão" },
-            { code: "A30", name: "Lesão corporal" },
-            { code: "A98", name: "Violência Doméstica" },
-            { code: "A99", name: "Medidas Protetivas (aplicativo)" }
-        ],
-        "Grupo B - Contra o Patrimônio": [
-            { code: "B01", name: "Furto" },
-            { code: "B02", name: "Furto tentativa" },
-            { code: "B04", name: "Roubo" },
-            { code: "B05", name: "Extorsão" },
-            { code: "B07", name: "Dano material" },
-            { code: "B11", name: "Latrocínio" }
-        ],
-        "Grupo E - Contra a Administração Pública": [
-            { code: "E01", name: "Peculato" },
-            { code: "E02", name: "Concussão" },
-            { code: "E03", name: "Corrupção" },
-            { code: "E08", name: "Desobediência" },
-            { code: "E09", name: "Desacato" }
-        ],
-        "Grupo L - De Trânsito": [
-            { code: "L01", name: "Veículo" },
-            { code: "L07", name: "Atropelamento" },
-            { code: "L08", name: "Acidente de trânsito com vítima" },
-            { code: "L09", name: "Acidente de trânsito sem vítima" }
-        ]
+        "Contra a Pessoa e a Vida": ["Homicídio culposo", "Homicídio doloso", "Homicídio tentado", "Agressão", "Lesão corporal", "Violência Doméstica"],
+        "Contra o Patrimônio": ["Furto", "Furto tentativa", "Roubo", "Extorsão", "Dano material", "Latrocínio"],
+        "Contra a Administração Pública": ["Peculato", "Concussão", "Corrupção", "Desobediência", "Desacato"],
+        "De Trânsito": ["Veículo", "Atropelamento", "Acidente de trânsito com vítima", "Acidente de trânsito sem vítima"]
     };
 
     function preencherSelect(selectId) {
@@ -46,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
             naturezas[grupo].forEach(item => {
                 const option = document.createElement("option");
-                option.value = item.code;
-                option.textContent = item.name;
+                option.value = item;
+                option.textContent = item;
                 optgroup.appendChild(option);
             });
 
